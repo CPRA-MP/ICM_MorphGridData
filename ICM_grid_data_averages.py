c6e2b76399bfd6228d446d29508123ea5564c500
Eric <47019383+edwhite12@users.noreply.github.com>
2022-04-24 23:40:10 -0500

    Update ICM_grid_data_averages.py

--- ICM_grid_data_averages.py
+++ ICM_grid_data_averages.py
@@ -1,4 +1,20 @@
+import os
+import sys
+
+mpterm = 'MP2023'
+sterm = 'S%02d' % int(sys.argv[1])
+gterm = 'G%03d' % int(sys.argv[2])
+cterm = 'C000'
+uterm = 'U00'
+vterm = 'V00'
+rterm = 'SLA'
+
+year = int(sys.argv[3])
+
+
 print('\nPreparing summary files for grid and compartment zonal statistics.')
+
+par_dir = os.getcwd()
 
 MorphGridData_exe_path = '%s/%s/%s/geomorph/morph_grid_data_v23.0.0' % (par_dir, sterm, gterm)
 
@@ -15,8 +31,8 @@ out_file  = '%s/%s/%s/geomorph/output/%s_O_%02d_%02d_W_grid_data.csv'           
 ndem = 171284090
 dem_NoDataVal = -9999
 dem_res = 30
-n500grid =
-ncomp = 
+n500grid = 173898
+ncomp = 1778
         
 fortran_run = subprocess.call([MorphGridData_exe_path, dem_file, lwf_file, edge_file, grid_file, comp_file, ndem, dem_NoDataVal])
 
@@ -104,20 +120,17 @@ with open(out_file,mode='r') as grid_data:
                 grid_pct_water_all[g].append(1)
                 comp_water_z_all[c].append(elev)
                 comp_pct_water_all[c].append(1)
-                
             else:
                 grid_land_z_all[g].append(elev)
                 grid_pct_land_all[g].append(1)
                 if lndtyp <> 4:     # check if upland/developed
                     grid_pct_land_wetl_all[g].append(1)
                     comp_wetland_z_all[c].append(elev)
-                    
                 else:
                     comp_pct_upland_all[c].append(1)
         
             grid_pct_edge_all[g].append(edge)    
             comp_edge_area_all[c].append(edge*dem_res*dem_res)
-            
             
         nline += 1
 
@@ -125,9 +138,12 @@ for g in range(1,n500grid+1):
     ng = len(grd_bed_z_all[g])
     
     if ng > 0:
-        grid_bed_z[g] = sum(grid_bed_z_all[g]) / ng
-    
-    
+        grid_bed_z[g]          = sum(grid_bed_z_all[g]) / ng
+        grid_land_z[g]         = sum(grid_land_z_all[g]) / ng
+        grid_pct_land[g]       = 100.0*sum(grid_pct_land_all[g]) / ng
+        grid_pct_land_wetl[g]  = 100.0*sum(grid_pct_land_wetl_all[g]) / ng
+        grid_pct_water[g]      = 100.0*sum(grid_pct_water_all[g]) / ng
+        grid_pct_edge[g]       = 100.0*sum(grid_pct_edge_all[g]) / ng
     else:
         grid_bed_z[g] = 0.0
 
@@ -137,36 +153,37 @@ for c in range(1,ncomp+1):
 
     if nc > 0:
         comp_pct_upland[c] = sum(comp_pct_upland_all[c]) / nc
+        comp_water_z[c]    = sum(comp_water_z_all[c]   ) / nc
+        comp_wetland_z[c]  = sum(comp_wetland_z_all[c] ) / nc
+        comp_pct_water[c]  = sum(comp_pct_water_all[c] ) / nc
+        comp_edge_area[c]  = sum(comp_edge_area_all[c] )
     else:
-        comp_pct_upland[c] = 0
+        comp_pct_upland[c] = 0.0
+        comp_water_z[c]    = 0.0
+        comp_wetland_z[c]  = 0.0
+        comp_pct_water[c]  = 0.0
+        comp_edge_area[c]  = 0.0        
         
-        
-        
-grid_bed_z[g]
-grid_land_z[g]
-100.0*grid_pct_land[g]
-100.0*grid_pct_land_wetl[g]
-100.0*grid_pct_water[g]
-100.0*grid_pct_edge[g]
 
 
-grid_data_file = new_grid_filepath
+new_grid_filepath  = 'hydro/TempFiles/grid_data_500m_end%s.csv'
+comp_elev_file     = 'hydro/TempFiles/compelevs_end_%d.csv' % year
+comp_wat_file      = 'hydro/TempFiles/PctWater_%d.csv' % year
+comp_upl_file      = 'hydro/TempFiles/PctUpland_%d.csv' % year
 grid_pct_edge_file = 'hsi/%s_W_pedge.csv' % file_prefix
-comp_elev_file = 'hydro/TempFiles/compelevs_end_%d.csv' % year
-comp_wat_file  = 'hydro/TempFiles/PctWater_%d.csv' % year
-comp_upl_file  = 'hydro/TempFiles/PctUpland_%d.csv' % year
-    
+
+grid_data_file     = new_grid_filepath  
 
 
 with open(grid_data_file,mode='w') as gdaf:  
     gdaf.write('GRID,MEAN_BED_ELEV,MEAN_LAND_ELEV,PERCENT_LAND_0-100,PERCENT_WETLAND_0-100,PERCENT_WATER_0-100\n')
     for g in grid_bed_z.keys():
-        gdaf.write('%d,%0.4f,%0.4f,%0.2f,%0.2f,%0.2f\n' % (g,grid_bed_z[g],grid_land_z[g],100.0*grid_pct_land[g],100.0*grid_pct_land_wetl[g],100.0*grid_pct_water[g]) )
+        gdaf.write('%d,%0.4f,%0.4f,%0.2f,%0.2f,%0.2f\n' % (g,grid_bed_z[g],grid_land_z[g],grid_pct_land[g],grid_pct_land_wetl[g],grid_pct_water[g]) )
     
 with open(grid_pct_edge_file,mode='w') as gdef:  
     gdef.write('GRID,PERCENT_EDGE_0-100\n')
     for g in grid_pct_edge.keys():
-        gdef.write('%d,%0.4f' % (g,100.0*grid_pct_edge[g]) )
+        gdef.write('%d,%0.4f' % (g,grid_pct_edge[g]) )
       
 with open(comp_elev_file,mode='w') as cef:
     cef.write('ICM_ID,MEAN_BED_ELEV,MEAN_MARSH_ELEV,MARSH_EDGE_AREA\n')
