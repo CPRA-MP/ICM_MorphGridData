defdc57e503530fd390444d9a3abdf9c0092b54e
Eric White <47019383+edwhite12@users.noreply.github.com>
2022-05-13 17:41:47 -0500

    add HSI bird depth tabulated areas to script

--- ICM_grid_data_averages.py
+++ ICM_grid_data_averages.py
@@ -35,9 +35,29 @@ dem_NoDataVal = -9999
 dem_res = 30
 n500grid = 173898
 ncomp = 1778
-        
+
+Gdw_bin_n = 14    # number of water depth bins used by Gadwall HSI
+GwT_bin_n = 9     # number of water depth bins used by Greenwing Teal HSI
+MtD_bin_n = 9     # number of water depth bins used by Mottled Duck HSI
+
+
 fortran_run = subprocess.call([MorphGridData_exe_path, dem_file, lwf_file, edge_file, grid_file, comp_file, str(ndem), str(dem_NoDataVal),out_file])
 print('Finished running %s.' % MorphGridData_exe_path)
+
+print('Reading in %s.' % monthly_stg_file)
+monthly_stg_file = '%s/%s/%s/hydro/TempFiles/compartment_monthly_mean_stage_%04d.csv'  % (par_dir, sterm, gterm, year)
+# comp,stage_m_01,stage_m_02,stage_m_03,stage_m_04,stage_m_05,stage_m_06,stage_m_07,stage_m_08,stage_m_09,stage_m_10,stage_m_11,stage_m_12
+comp_mon_stg = {}
+with open(monthly_stg_file,mode='r') as comp_stg_data:
+    nline = 0
+    for line in comp_stg_data:
+        if nline > 0:
+            c = int(line.split(',')[0])
+            comp_mon_stg[c] = {}
+            for n_mon in range(1,13):
+                comp_mon_stg[c][n_mon] = int(line.split(',')[n_mon])
+    nline += 1
+
 print('Reading in %s.' % out_file)
 
 grid_bed_z_all = {}
@@ -57,6 +77,10 @@ grid_pct_water = {}
 
 grid_pct_edge_all = {}
 grid_pct_edge = {}
+
+grid_Gdw_depths = {}
+grid_GwT_depths = {}
+grid_MtD_depths = {}
 
 comp_water_z_all = {}
 comp_water_z = {}
@@ -91,6 +115,18 @@ for g in range(1,n500grid+1):
     
     grid_pct_edge_all[g] = []      
     grid_pct_edge[g] = 0.0
+
+    grid_Gdw_depths[g] = []
+    for Gdw_bin in range(0,Gdw_bin_n + 1):
+        grid_Gdw_depths[g][Gdw_bin] = 0.0
+        
+    grid_GwT_depths[g] = []
+    for GwT_bin in range(0,GwT_bin_n + 1):
+        grid_GwT_depts[g][GwT_bin] = 0.0
+        
+    grid_MtD_depths[g] = []    
+    for MtD_bin in range(0,MtD_bin_n + 1):
+        grid_MtD_depths[g][MtD_bin] = 0.0
     
 for c in range(1,ncomp+1):                           
     comp_water_z_all[c] = []       
@@ -126,17 +162,6 @@ with open(out_file,mode='r') as grid_data:
             edge   = int(float(line.split(',')[4]))
             elev   = float(line.split(',')[5])
             
-            if g > 0:
-                grid_pct_edge_all[g].append(edge)
-                if lndtyp == 2:
-                    grid_bed_z_all[g].append(elev)
-                    grid_pct_water_all[g].append(1)
-                else:
-                    grid_land_z_all[g].append(elev)
-                    grid_pct_land_all[g].append(1)
-                    if lndtyp != 4:     # check if upland/developed
-                        grid_pct_land_wetl_all[g].append(1)
-                        
             if c > 0:
                 comp_edge_area_all[c].append(edge*dem_res*dem_res)
                 if lndtyp == 2:
@@ -147,8 +172,96 @@ with open(out_file,mode='r') as grid_data:
                         comp_wetland_z_all[c].append(elev)
                     else:
                         comp_pct_upland_all[c].append(1)
+            
+            if g > 0:
+                grid_pct_edge_all[g].append(edge)
+                if lndtyp == 2:
+                    grid_bed_z_all[g].append(elev)
+                    grid_pct_water_all[g].append(1)
+                else:
+                    grid_land_z_all[g].append(elev)
+                    grid_pct_land_all[g].append(1)
+                    if lndtyp != 4:     # check if upland/developed
+                        grid_pct_land_wetl_all[g].append(1)
+                
+                # tabulate area of grid cell within each Gadwall depth bin; depth thresholds (in m) are: [0,0.04,0.08,0.12,0.18,0.22,0.28,0.32,0.36,0.40,0.44,0.78,1.50]
+                if c > 0:
+                    dep_oct_apr = (comp_mon_stg[c][1]+comp_mon_stg[c][2]+comp_mon_stg[c][3]+comp_mon_stg[c][4]+comp_mon_stg[c][10]+comp_mon_stg[c][11]+comp_mon_stg[c][12])/7.0
+                    dep_sep_mar = (comp_mon_stg[c][1]+comp_mon_stg[c][2]+comp_mon_stg[c][3]+comp_mon_stg[c][9]+comp_mon_stg[c][10]+comp_mon_stg[c][11]+comp_mon_stg[c][12])/7.0
+                    dep_ann     = (comp_mon_stg[c][1]+comp_mon_stg[c][2]+comp_mon_stg[c][3]+comp_mon_stg[c][4]+comp_mon_stg[c][5]+comp_mon_stg[c][6]+comp_mon_stg[c][7]+comp_mon_stg[c][8]+comp_mon_stg[c][9]+comp_mon_stg[c][10]+comp_mon_stg[c][11]+comp_mon_stg[c][12])/12.0
+                    
+                    if dep_oct_apr <= 0.0:
+                        grid_Gdw_depths[g][1]  = grid_Gdw_depths[g][1] + dem_res**2
+                    elif dep_oct_apr <= 0.04:
+                        grid_Gdw_depths[g][2]  = grid_Gdw_depths[g][2] + dem_res**2
+                    elif dep_oct_apr <= 0.08:
+                        grid_Gdw_depths[g][3]  = grid_Gdw_depths[g][3] + dem_res**2
+                    elif dep_oct_apr <= 0.12:
+                        grid_Gdw_depths[g][4]  = grid_Gdw_depths[g][4] + dem_res**2
+                    elif dep_oct_apr <= 0.18:
+                        grid_Gdw_depths[g][5]  = grid_Gdw_depths[g][5] + dem_res**2
+                    elif dep_oct_apr <= 0.22:
+                        grid_Gdw_depths[g][6]  = grid_Gdw_depths[g][6] + dem_res**2
+                    elif dep_oct_apr <= 0.28:
+                        grid_Gdw_depths[g][7]  = grid_Gdw_depths[g][7] + dem_res**2
+                    elif dep_oct_apr <= 0.32:
+                        grid_Gdw_depths[g][8]  = grid_Gdw_depths[g][8] + dem_res**2
+                    elif dep_oct_apr <= 0.36:
+                        grid_Gdw_depths[g][9]  = grid_Gdw_depths[g][9] + dem_res**2
+                    elif dep_oct_apr <= 0.40:
+                        grid_Gdw_depths[g][10] = grid_Gdw_depths[g][10] + dem_res**2
+                    elif dep_oct_apr <= 0.44:
+                        grid_Gdw_depths[g][11] = grid_Gdw_depths[g][11] + dem_res**2
+                    elif dep_oct_apr <= 0.78:
+                        grid_Gdw_depths[g][12] = grid_Gdw_depths[g][12] + dem_res**2
+                    elif dep_oct_apr <= 1.50:
+                        grid_Gdw_depths[g][13] = grid_Gdw_depths[g][13] + dem_res**2
+                    else
+                        grid_Gdw_depths[g][14] = grid_Gdw_depths[g][14] + dem_res**2
+                    
+                    # tabulate area of grid cell within each Greenwing Teal depth bin; depth thresholds (in m) are: [0,0.06,0.18,0.22,0.26,0.30,0.34,1.0]
+                    if dep_sep_mar <= 0.0:
+                        grid_GwT_depths[g][1] = grid_GwT_depths[g][1] + dem_res**2
+                    elif dep_sep_mar <= 0.06:
+                        grid_GwT_depths[g][2] = grid_GwT_depths[g][2] + dem_res**2
+                    elif dep_sep_mar <= 0.18:
+                        grid_GwT_depths[g][3] = grid_GwT_depths[g][3] + dem_res**2
+                    elif dep_sep_mar <= 0.22:
+                        grid_GwT_depths[g][4] = grid_GwT_depths[g][4] + dem_res**2
+                    elif dep_sep_mar <= 0.26:
+                        grid_GwT_depths[g][5] = grid_GwT_depths[g][5] + dem_res**2
+                    elif dep_sep_mar <= 0.30:
+                        grid_GwT_depths[g][6] = grid_GwT_depths[g][6] + dem_res**2
+                    elif dep_sep_mar <= 0.34:
+                        grid_GwT_depths[g][7] = grid_GwT_depths[g][7] + dem_res**2
+                    elif dep_sep_mar <= 1.0:
+                        grid_GwT_depths[g][8] = grid_GwT_depths[g][8] + dem_res**2
+                    else
+                        grid_GwT_depths[g][9] = grid_GwT_depths[g][9] + dem_res**2
+                       
+                    # tabulate area of grid cell within each Mottled Duck depth bin; depth thresholds (in m) are: [0,0.08,0.30,0.36,0.42,0.46,0.50,0.56]
+                    if dep_ann <= 0.0:        
+                        grid_MtD_depths[g][1] = grid_MtD_depths[g][1] + dem_res**2
+                    elif dep_ann <= 0.08:            
+                        grid_MtD_depths[g][2] = grid_MtD_depths[g][2] + dem_res**2
+                    elif dep_ann <= 0.30:            
+                        grid_MtD_depths[g][3] = grid_MtD_depths[g][3] + dem_res**2
+                    elif dep_ann <= 0.36:            
+                        grid_MtD_depths[g][4] = grid_MtD_depths[g][4] + dem_res**2
+                    elif dep_ann <= 0.42:            
+                        grid_MtD_depths[g][5] = grid_MtD_depths[g][5] + dem_res**2
+                    elif dep_ann <= 0.46:            
+                        grid_MtD_depths[g][6] = grid_MtD_depths[g][6] + dem_res**2
+                    elif dep_ann <= 0.50] then            
+                        grid_MtD_depths[g][7] = grid_MtD_depths[g][7] + dem_res**2
+                    elif dep_ann <= 0.56:            
+                        grid_MtD_depths[g][8] = grid_MtD_depths[g][8] + dem_res**2
+                    else
+                        grid_MtD_depths[g][9] = grid_MtD_depths[g][9] + dem_res**2
+
         nline += 1
 
+# determine zonal averages over each ICM-LAVegMod grid cell
 for g in range(1,n500grid+1):
     ng = len(grid_bed_z_all[g]) + len(grid_land_z_all[g])
     
@@ -166,9 +279,9 @@ for g in range(1,n500grid+1):
         grid_pct_land_wetl[g]  = 0.0
         grid_pct_water[g]      = 0.0
         grid_pct_edge[g]       = 0.0
+        
 
-
-
+# determine zonal averages over each ICM-Hydro compartment
 for c in range(1,ncomp+1):    
     nc = len(comp_water_z_all[c]) + len(comp_wetland_z_all[c]) + len(comp_pct_upland_all[c])
 
@@ -186,12 +299,16 @@ for c in range(1,ncomp+1):
         comp_edge_area[c]  = 0.0        
         
 
+# output files
+new_grid_filepath   = '%s/%s/%s/hydro/TempFiles/grid_data_500m_end%d.csv' % (par_dir, sterm, gterm, year)
+comp_elev_file      = '%s/%s/%s/hydro/TempFiles/compelevs_end_%d.csv' % (par_dir, sterm, gterm, year)
+comp_wat_file       = '%s/%s/%s/hydro/TempFiles/PctWater_%d.csv' % (par_dir, sterm, gterm, year)
+comp_upl_file       = '%s/%s/%s/hydro/TempFiles/PctUpland_%d.csv' % (par_dir, sterm, gterm, year)
+grid_pct_edge_file  = '%s/%s/%s/hsi/%s_W_pedge.csv' % (par_dir, sterm, gterm, file_prefix)
 
-new_grid_filepath  = '%s/%s/%s/hydro/TempFiles/grid_data_500m_end%d.csv' % (par_dir, sterm, gterm, year)
-comp_elev_file     = '%s/%s/%s/hydro/TempFiles/compelevs_end_%d.csv' % (par_dir, sterm, gterm, year)
-comp_wat_file      = '%s/%s/%s/hydro/TempFiles/PctWater_%d.csv' % (par_dir, sterm, gterm, year)
-comp_upl_file      = '%s/%s/%s/hydro/TempFiles/PctUpland_%d.csv' % (par_dir, sterm, gterm, year)
-grid_pct_edge_file = '%s/%s/%s/hsi/%s_W_pedge.csv' % (par_dir, sterm, gterm, file_prefix)
+grid_Gdw_dep_file   = '%s/%s/%s/hsi/GadwallDepths_cm_%d.csv' % (par_dir, sterm, gterm, year)
+grid_GwT_dep_file   = '%s/%s/%s/hsi/GWTealDepths_cm__%d.csv' % (par_dir, sterm, gterm, year)
+grid_MtD_dep_file   = '%s/%s/%s/hsi/MotDuckDepths_cm_%d.csv' % (par_dir, sterm, gterm, year)
 
 grid_data_file     = new_grid_filepath  
 
@@ -223,3 +340,27 @@ with open(comp_upl_file, mode='w') as cuf:
     print('     - %s' % comp_upl_file)
     for c in comp_pct_upland.keys():
         cuf.write( '%d,%0.4f\n' % (c,comp_pct_upland[c]) )
+
+with open(grid_Gdw_dep_file, mode='w') as Gdw:    
+    Gdw.write('GRID_ID,VALUE_0,VALUE_4,VALUE_8,VALUE_12,VALUE_18,VALUE_22,VALUE_28,VALUE_32,VALUE_36,VALUE_40,VALUE_44,VALUE_78,VALUE_150,VALUE_151\n')
+    for g in grid_Gdw_depths.keys():
+        linewrite = '%d' % g
+        for Gdw_bin in range(0,Gdw_bin_n + 1):
+            linewrite = '%s,%d' % (linewrite,grid_Gdw_depths[g][Gdw_bin])
+        Gdw.write('%s\n' % linewrite)
+
+with open(grid_GwT_dep_file, mode='w') as GwT:
+    GwT.write('GRID_ID,VALUE_0,VALUE_6,VALUE_18,VALUE_22,VALUE_26,VALUE_30,VALUE_34,VALUE_100,VALUE_101\n')
+    for g in grid_GwT_depths.keys():
+        linewrite = '%d' % g
+        for GwT_bin in range(0,GwT_bin_n + 1):
+            linewrite = '%s,%d' % (linewrite,grid_GwT_depths[g][GwT_bin])
+        GwT.write('%s\n' % linewrite)
+
+with open(grid_MtD_dep_file, mode='w') as MtD:
+    MtD.write('GRID_ID,VALUE_0,VALUE_8,VALUE_30,VALUE_36,VALUE_42,VALUE_46,VALUE_50,VALUE_56,VALUE_57\n')
+    for g in grid_MtD_depths.keys():
+        linewrite = '%d' % g
+        for MtD_bin in range(0,MtD_bin_n + 1):
+            linewrite = '%s,%d' % (linewrite,grid_MtD_depths[g][MtD_bin])
+        MtD.write('%s\n' % linewrite)
